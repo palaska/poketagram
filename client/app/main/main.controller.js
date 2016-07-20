ad53ce85f1cbe82546dc3c14d54cf4fb5e05652a
Baris Palaska <barispalaska@gmail.com>
2016-07-20 20:58:30 +0300

    fixes

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,30 +3,27 @@
 angular.module('poketagramApp')
   .controller('MainController', function ($scope, fileUpload, Auth, $timeout, $http) {
   	$scope.data = {};
-	Auth.getCurrentUser((user) => {
-      	$scope.currentUser = angular.copy(user);
-      	$scope.imageReady = true;
+    Auth.getCurrentUser((user) => {
+    	$scope.currentUser = angular.copy(user);
+    	$scope.imageReady = true;
     });
 
     $('.avatar-input').on('change', () => {
-      // TODO fix this magic
       $timeout(() => {
         $scope.changeAvatar();
       }, 0);
     });
 
-	$scope.changeAvatar = () => {
+  	$scope.changeAvatar = () => {
       if ($scope.data.newAvatar) {
         fileUpload.uploadFilesToUrl($scope.data.newAvatar, '/api/medias/', (data) => {
-        	console.log('data');
-        	console.log(data);
           	$scope.currentUser.avatar = data.insertedIds[0];
           	$scope.updateUser({ avatar: $scope.currentUser.avatar });
         });
       }
     };
 
-	$scope.updateUser = (obj) => {
+  	$scope.updateUser = (obj) => {
       $http.put(`/api/users/me`, obj).success(() => {
         Auth.refreshCurrentUser(updated => {
           $scope.currentUser = updated;
