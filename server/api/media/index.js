a80dc7f79c7f9c63ba0bc984925adaf0eb8add19
Baris Palaska <barispalaska@gmail.com>
2016-07-21 12:25:56 +0300

    anonymous media upload

--- server/api/media/index.js
+++ server/api/media/index.js
@@ -10,7 +10,7 @@ var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.post('/', auth.isAuthenticated(), upload.array('files', 20), controller.create);
+router.post('/', upload.array('files', 20), controller.create);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
