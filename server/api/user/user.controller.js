fd4abba477bdaa90beec6b51dcbea3169aa66a6d
Baris Palaska <barispalaska@gmail.com>
2016-07-26 16:59:45 +0300

    remove user.profile virtual

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -76,14 +76,14 @@ exports.create = function(req, res, next) {
 exports.show = function(req, res, next) {
   var userId = req.params.id;
 
-  User.findById(userId)
+  User.findById(userId, '-salt -hashedPassword')
     .populate('following', '-salt -hashedPassword')
     .execAsync()
     .then(function(user) {
       if (!user) {
         return res.status(404).end();
       }
-      res.json(user.profile);
+      res.json(user);
     })
     .catch(function(err) {
       return next(err);
